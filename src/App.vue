52c3d980d8de901d8f660eca1cd2e4e828e1c7aa
Anna Kotowicz <annakodowicz@gmail.com>
2021-05-29 21:17:36 +0200

    initialize vue app

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>rick and morty</h1>
+</template>
+
+<style>
+body {
+  margin: 0;
+}
+#app {
+  font-family: "Poppins", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size: 1rem;
+}
+
+* {
+  box-sizing: border-box;
+}
+</style>
